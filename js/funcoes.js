169dbddd17c099820a54282b05ac8e2baabbc849
marcelo vinicius <viniciusmarcello12@gmail.com>
2021-04-06 22:21:26 -0300

    adição de imagem atraves do link de inserir

---
+++ js/funcoes.js
@@ -0,0 +1,16 @@
+function AdicionarFilme(){
+    let campoFilme = document.querySelector("#filme");
+    let imagemFilme = campoFilme.value;
+    
+    if(imagemFilme.endsWith(".jpg")){
+   ListarFilmesNaTela(imagemFilme);
+}else{
+    alert("isso não é uma imagem!!!!");
+}
+}
+function ListarFilmesNaTela(imagem){
+let listaFilmes = document.querySelector("#listaFilmes");
+let elementoFilme = "<img src=" +  imagem + ">";
+console.log(elementoFilme);
+listaFilmes.innerHTML += elementoFilme;
+}
